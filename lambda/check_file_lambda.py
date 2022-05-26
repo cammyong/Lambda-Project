96321c040cc9861b2bb1d28d32f0bdb7db9d8d07
Ubong Michael <princewhobong@yahoo.com>
2022-05-26 16:49:48 +0200

    checking if file exits in s3

--- lambda/check_file_lambda.py
+++ lambda/check_file_lambda.py
@@ -1,4 +1,9 @@
 import logging
+import boto3
+
+s3_client = boto3.client('s3')
+bucket = "s3-lambda-copy-src-bucket1"
+key = "file2.txt"
 
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
@@ -8,5 +13,7 @@ def handler(event, context):
     LOGGER.info(f'Event Object: {event}')
     LOGGER.info(f'Context Object: {context}')
     LOGGER.info("Check File")
+    response = s3_client.get_object(Bucket=bucket, Key=key)  
+    data = response['Body'].read() 
     event['key'] = 'value'
     return event
